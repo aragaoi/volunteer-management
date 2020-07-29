e952bab5a2f5c39d573c0147ffacf5a5f62c95d7
iaragao <irenio.limaj@hotmail.com>
2020-07-29 13:27:52 -0300

    fix password form

--- src/views/Settings/components/Password/PasswordForm.js
+++ src/views/Settings/components/Password/PasswordForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {makeStyles} from '@material-ui/styles';
@@ -20,19 +20,18 @@ const PasswordForm = props => {
 
   const {register, errors} = useFormContext();
 
-  const handleChangePassword = event => {
-    event.stopPropagation();
+  useEffect(() => {
+    props.onChange(password);
+  }, [password])
 
+  const handleChangePassword = event => {
     const value = event.target.value;
-    setPassword(value);
-    props.onChange(value);
+    setPassword(() => value);
   };
 
   const handleChangeConfirm = event => {
-    event.stopPropagation();
-
     const value = event.target.value;
-    setConfirm(value);
+    setConfirm(() => value);
   };
 
   return (
@@ -40,58 +39,56 @@ const PasswordForm = props => {
       {...rest}
       className={clsx(classes.root, className)}
     >
-      <form>
-        <CardHeader
-          title="Senha"
-        />
-        <Divider/>
-        <CardContent>
+      <CardHeader
+        title="Senha"
+      />
+      <Divider/>
+      <CardContent>
+        <Grid
+          container
+          spacing={3}
+        >
           <Grid
-            container
-            spacing={3}
+            item
+            md={6}
+            xs={12}
           >
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Senha"
-                name="password"
-                inputRef={register}
-                required
-                margin="dense"
-                onChange={handleChangePassword}
-                type="password"
-                value={password}
-                variant="outlined"
-              />
-              <ErrorMessage errors={errors} name="password" />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Confirme a senha"
-                name="confirm"
-                inputRef={register}
-                required
-                margin="dense"
-                onChange={handleChangeConfirm}
-                // style={{ marginTop: '1rem' }}
-                type="password"
-                value={confirm}
-                variant="outlined"
-              />
-              <ErrorMessage errors={errors} name="confirm" />
-            </Grid>
+            <TextField
+              fullWidth
+              label="Senha"
+              name="password"
+              inputRef={register}
+              required
+              margin="dense"
+              onChange={handleChangePassword}
+              type="password"
+              value={password}
+              variant="outlined"
+            />
+            <ErrorMessage errors={errors} name="password"/>
           </Grid>
-        </CardContent>
-      </form>
+          <Grid
+            item
+            md={6}
+            xs={12}
+          >
+            <TextField
+              fullWidth
+              label="Confirme a senha"
+              name="confirm"
+              inputRef={register}
+              required
+              margin="dense"
+              onChange={handleChangeConfirm}
+              // style={{ marginTop: '1rem' }}
+              type="password"
+              value={confirm}
+              variant="outlined"
+            />
+            <ErrorMessage errors={errors} name="confirm"/>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
